b18d8d06768417704c57a601d5c8314387c70627
Abdulrahman Laptop <.hamani7115@gmail.com>
2026-09-24 03:06:24 +0300

    debug hip bash

--- build_hip.sh
+++ build_hip.sh
@@ -9,6 +9,7 @@ SOURCE="${1:-tests/create_destory_graph_hip.cpp}"
 OUTPUT="${2:-create_destory_graph_hip.out}"
 
 "${HIPCC}" \
+    --gcc-toolchain=/opt/rh/gcc-toolset-13/root/usr \
     -std=c++20 \
     -O2 \
     -g \
